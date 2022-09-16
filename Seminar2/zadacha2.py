96222d1a8869fda24f2a74b44521719d7a0d4386
Petr_Abramovich <malyarpetr@gmail.com>
2022-09-16 13:59:09 +0300

    Внесены изменения в задачи 2 и 3 Решены через цикл while

--- Seminar2/zadacha2.py
+++ Seminar2/zadacha2.py
@@ -1,20 +1,16 @@
 #Требуется посчитать сумму целых чисел, расположенных между числами 1 и N включительно.
 
-def array_of_integer(n):
-    nums = []
-    for i in range(1,n+1):
-        nums.append(i)
-    return nums
-    
-def sum_of_integers(arr):
+def sum(n):
+    i = 1
     sum = 0
-    for e in range(len(arr)):
-        sum = sum + arr[e]
+    while i <= n:
+        sum = sum + i
+        i += 1
     return sum
 
 n = int(input('Введите число: '))
 while n <= 0 or n > 10 ** 4:
     print('Число должно быть больше нуля и меньше 10000')
     n = int(input('Введите число: '))
-result = sum_of_integers(array_of_integer(n))
+result = sum(n)
 print(f'Сумма целых чисел от 1 до {n} : {result}')
